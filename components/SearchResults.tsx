d262c6a49bdaa3a1c3a61494b36ce0fc940d4c3d
Vinicius Mendite <vinicius@sharbe.com.br>
2021-06-17 15:05:13 -0300

    react virtualized

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
 
 import { ProductItem } from './ProductItem';
 
@@ -17,15 +18,32 @@ export function SearchResults({ results }: SearchResultsProps) {
     }, 0)
   }, [results])
 
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem product={results[index]} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => {
         return (
           <ProductItem key={product.id} product={product} />
         );
-      })}
+      })} */}
     </div>
   );
 }
